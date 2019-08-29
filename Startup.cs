2ba607d2094607634fcc5e82e97612c89404f461
Kelar Crisp <kelcrisp007@icloud.com>
2019-08-28 23:02:13 -0400

    added swagger

--- Startup.cs
+++ Startup.cs
@@ -26,18 +26,23 @@ namespace projectAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddCors();
+
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddSwaggerGen(c =>
+                   {
+                       c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+                   });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-
-            app.UseCors(x => x
-                .AllowAnyOrigin()
-                .AllowAnyMethod()
-                .AllowAnyHeader());
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+                c.RoutePrefix = string.Empty;
+            });
 
             if (env.IsDevelopment())
             {
